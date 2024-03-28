8d2554e44c15719419cf7c59716ebda8f5c991bc
rjcalifornia <rodolfohernan20@gmail.com>
2024-03-27 21:00:43 -0600

    Changes to be committed: 	modified:   app/Http/Controllers/api/TransmisionController.php

--- app/Http/Controllers/api/TransmisionController.php
+++ app/Http/Controllers/api/TransmisionController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\ActaElectoral;
+use App\Models\Dispositivos;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class TransmisionController extends Controller
 {
@@ -15,7 +18,31 @@ class TransmisionController extends Controller
         return response()->json(['transmisiones'=> $actas], 200);
     }
 
-    public function almacenarTransmision(){
+    public function almacenarTransmision(Request $request){
+        $validator = Validator::make($request->all(), [
+            'id_junta_receptora' => 'integer|required',
+            'id_dispositivo' =>  'integer|required',
+            'sobrantes' =>  'integer|required',
+            'inutilizados' =>  'integer|required',
+            'impugnados' =>  'integer|required',
+            'nulos' =>  'integer|required',
+            'abstenciones' =>  'integer|required',
+            'escrutados' =>  'integer|required',
+            'faltantes' =>  'integer|required',
+            'entregados' =>  'integer|required',
+            'id_tipo_acta' =>  'integer|required',
+            'partidos' => 'array'
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = Auth::user();
+        $dispositivo = Dispositivos::where('id_usuario', $user->id)->first();
+
+        $acta = new ActaElectoral;
+        $acta->id_junta_receptora = $request->id_junta_receptora;
+        $acta->save();
     }
 }
